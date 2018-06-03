286d76100d0e1eb7db752077fd624467bfbd71da
adamgibbons <adam.d.gibbons@gmail.com>
2018-06-02 22:00:56 -0600

    format consumer debt

--- src/components/User/ConsumerDebt.vue
+++ src/components/User/ConsumerDebt.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <h4 class="title is-4">Consumer Debt</h4>
-    <pre>{{consumerDebt}}</pre>
+    <div
+      class="box"
+      v-for="({type, minMonthlyPayment, averageLoanBalance, interestRate}, index) in consumerDebt"
+      :key="index">
+      <div class="admin-field">
+        <div class="label">Type</div>
+        <div class="value">{{type || '-'}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Minimum Monthly Payment</div>
+        <div class="value">{{minMonthlyPayment | dollars}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Average Loan Balance</div>
+        <div class="value">{{averageLoanBalance | dollars}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Interest Rate</div>
+        <div class="value">{{interestRate}}%</div>
+      </div>
+    </div>
   </div>
 </template>
 
