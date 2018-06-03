2b43a6e97c56b22d910af477be2d4879e567ea27
adamgibbons <adam.d.gibbons@gmail.com>
2018-06-02 22:06:00 -0600

    add student loans

--- src/components/User/StudentLoans.vue
+++ src/components/User/StudentLoans.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
     <h4 class="title is-4">Student Loans</h4>
-    <pre>{{studentLoans}}</pre>
+    <div
+      class="box"
+      v-for="({type, minMonthlyPayment, graduationDate, school, balance}, index) in studentLoans"
+      :key="index">
+      <div class="admin-field">
+        <div class="label">Type</div>
+        <div class="value">{{type || '-'}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Minimum Monthly Payment</div>
+        <div class="value">{{minMonthlyPayment | dollars}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Graduation Date</div>
+        <div class="value">{{graduationDate}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">School</div>
+        <div class="value">{{school}}</div>
+      </div>
+      <div class="admin-field">
+        <div class="label">Balance</div>
+        <div class="value">{{balance | dollars}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
