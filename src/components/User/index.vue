d0544323522d192090f7354f03e9f93ef17728b6
adamgibbons <adam.d.gibbons@gmail.com>
2018-06-09 20:19:35 -0600

    add header to user details view

--- src/components/User/index.vue
+++ src/components/User/index.vue
@@ -1,6 +1,49 @@
 <template>
   <div>
-    <h3 class="title is-3">User: {{selectedUser.name}}</h3>
+    <h3 class="title is-3">User Details</h3>
+
+    <div class="columns">
+      <div class="column is-narrow">
+        <div class="admin-field header">
+          <div class="label">
+            Name
+          </div>
+          <div class="value">
+            {{selectedUser.name}}
+          </div>
+        </div>
+      </div>
+      <div class="column is-narrow">
+        <div class="admin-field header">
+          <div class="label">
+            Email
+          </div>
+          <div class="value">
+            {{selectedUser.email}}
+          </div>
+        </div>
+      </div>
+      <div class="column is-narrow">
+        <div class="admin-field header">
+          <div class="label">
+            Created
+          </div>
+          <div class="value">
+            {{selectedUser.createdAt || 'n/a'}}
+          </div>
+        </div>
+      </div>
+      <div class="column is-narrow">
+        <div class="admin-field header">
+          <div class="label">
+            ID
+          </div>
+          <div class="value">
+            {{selectedUser._id}}
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="tabs">
       <ul>
@@ -38,14 +81,25 @@ export default {
   .admin-field {
     margin: 1em 0;
   }
+
+  .admin-field.header {
+    border: 1px solid #ddd;
+    padding: 1em;
+    margin: 0;
+  }
+
+  .admin-field .label {
+    border-bottom: 1px dotted #ddd;
+  }
+
   .admin-field .label,
   .admin-field .label:not(:last-child) {
     margin-bottom: 0;
-    font-size: 1em;
+    font-size: 0.95em;
     font-weight: 400;
   }
   .admin-field .value {
-    font-size: 1.33em;
+    font-size: 1.3em;
     font-weight: 600;
   }
   h4.title.is-4 {
